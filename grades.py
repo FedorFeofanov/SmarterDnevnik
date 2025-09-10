baf22be935cc3485679212ab85be10bdcf8ef113
FedorFeofanov <fedor.feofanov@gmail.com>
2025-09-10 11:17:57 +0200

    Headless browser is now used

--- grades.py
+++ grades.py
@@ -6,15 +6,17 @@ import time
 def PrintGrades(driver):
     dropdown_toggle = driver.find_element(By.CLASS_NAME, "dropdown-toggle")
     dropdown_toggle.click()
-    grades_elem = driver.find_element(By.PARTIAL_LINK_TEXT, "Ukupni")
+    grades_elem = driver.find_element(By.XPATH, "/html/body/app-root/header/main-navbar/div[2]/div/div/parent-student-nav/div/div[2]/ul/li[1]/ul/li[1]/a")
     grades_elem.click()
     driver.implicitly_wait(10)
     table = driver.find_element(By.XPATH, "/html/body/app-root/div/ng-component/div/form/div[2]/trimester-partial/div[2]/div/div/table")
     tbody = table.find_element(By.TAG_NAME, "tbody")
     rows = tbody.find_elements(By.TAG_NAME, "tr")
-    print("_______________________________________________")
+    grade_matrix = []
     for row in rows:
         cells = row.find_elements(By.TAG_NAME, "td")
-        print("| Predmet: " + cells[0].text + " | Ocene: " + cells[1].text + " | Prosecna ocena: " + cells[2].text) 
-    print("_______________________________________________")
+        school_class = [cell.text for cell in cells]
+        grade_matrix.append(school_class)
     driver.get("https://smartdnevnik.smart.edu.rs/login")
+    return grade_matrix
+
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time, subprocess
+import time
 import login, grades
 
-#opts = webdriver.ChromeOptions()
-#opts.add_argument("--window-size=1920,1080")
-#driver = webdriver.Chrome(options=opts)
-driver = webdriver.Firefox()
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
+driver.set_window_size(1920, 1080)
+
 driver.get("https://smartdnevnik.smart.edu.rs/login")
-assert "SGMSClient" in driver.title
 while(True):
     login_input = input("Login: ")
     password_input = input("Password: ")
     if(login_input and password_input):
         login.Login(driver, login_input, password_input)
         driver.implicitly_wait(10)
-        grades.PrintGrades(driver)
+        print(grades.PrintGrades(driver))
 driver.implicitly_wait(10)
 driver.close()
