744b6ea6bf1b1d9b630767c113db135250cf60ad
fedor <fedor.feofanov@gmail.com>
2025-09-15 12:23:58 +0200

    Form login and password

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -13,9 +13,33 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/grades/", response_class=HTMLResponse)
+async def grades_report(request: Request, grades):
+    return templates.TemplateResponse(
+        request=request, name="grades.html", context={"grades": grades}
+    )
 
-@app.get("/grades/{login}/{password}", response_class=HTMLResponse)
-async def get_grades(request: Request, login: str, password: str):
+#@app.get("/grades/{login}/{password}", response_class=HTMLResponse)
+#async def get_grades(request: Request, login: str, password: str):
+    #grades = fetch_grades(driver=driver, login=login, password=password)
+    #return templates.TemplateResponse(
+        #request=request, name="grades.html", context={"grades": grades}
+    #)
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={}
+    )
+
+#@app.post("/sign_in/")
+#async def sign_in(request: Request):
+    #print(request.login)
+    #grades = fetch_grades(driver=driver, login=login, password=password)
+    #return grades_report(request, login, password, grades)
+
+@app.post("/sign_in/")
+async def sign_in(request: Request, login: str = Form(), password: str = Form()):
     grades = fetch_grades(driver=driver, login=login, password=password)
     return templates.TemplateResponse(
         request=request, name="grades.html", context={"grades": grades}
